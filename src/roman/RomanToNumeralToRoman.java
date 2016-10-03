b42a2de07010bfbc947d50f72e2ee6edc48699a0
duhlarik <DeeAnn.Uhlarik@gmail.com>
2016-10-03 17:58:24 -0400

    FINALLY able to refactor the way I wanted to. Discovered that V stands for both 5 and 5000. Removed HashMap and used 2 ArrayLists instead.

--- src/roman/RomanToNumeralToRoman.java
+++ src/roman/RomanToNumeralToRoman.java
@@ -1,53 +1,14 @@
 package roman;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class RomanToNumeralToRoman {
 
-	Map<String, Integer> RomanToNumber;
-
-	public RomanToNumeralToRoman() {
-		RomanToNumber = new HashMap<String, Integer>();
-		RomanToNumber.put("I", 1);
-		RomanToNumber.put("V", 5);
-		RomanToNumber.put("X", 10);
-		RomanToNumber.put("L", 50);
-		RomanToNumber.put("C", 100);
-		RomanToNumber.put("D", 500);
-		RomanToNumber.put("M", 1000);
-	}
-
-	public int toNumber(String roman) {
-		int result = 0;
-		for (int i = 0; i < roman.length(); i++) {
-			int right = rightRoman(roman, i);
-			String currentRoman = roman.substring(i, i+1);
-			int current = RomanToNumber.get(currentRoman);
-			if (right > current) {
-				current *= -1;
-			}
-			result += current;
-		}
-		return result;
-	}
-
-	public int rightRoman(String roman, int i) {
-		int right = 0;
-		if (i < roman.length() - 1) {
-			String rightRoman = roman.substring(i + 1, i + 2);
-			right = RomanToNumber.get(rightRoman);
-		}
-		return right;
-	}
+	int[] numbers = { 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000, 4000 };
+	String[] romans = { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M", "MV" };
 
 	public String toRoman(int number) {
-		
+
 		String roman = "";
-		
-		int[] numbers = {1,4,5,9,10,40,50,90,100,400,500,900,1000,4000,5000};
-		String[] romans = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M", "MV", "V"};
-		
+
 		while (number > 0) {
 			int hit = 0;
 			for (int i = 0; i < numbers.length; i++) {
@@ -59,5 +20,46 @@ public class RomanToNumeralToRoman {
 			number -= numbers[hit];
 		}
 		return roman;
+	}
+
+	public int toNumber(String roman) {
+
+		int result = 0;
+		String currentRoman = "";
+		int currentHit = 0;
+		int current = 0;
+		String rightRoman = "";
+		int rightHit = 0;
+		int right = 0;
+
+		for (int i = 0; i < roman.length(); i++) {
+			currentRoman = roman.substring(i, i + 1);
+
+			for (int j = 0; j < romans.length; j++) {
+
+				if (romans[j].equalsIgnoreCase(currentRoman)) {
+					currentHit = j;
+				}
+				current = numbers[currentHit];
+			}
+
+			if (i < roman.length() - 1) {
+				rightRoman = roman.substring(i + 1, i + 2);
+
+				for (int k = 0; k < romans.length; k++) {
+
+					if (romans[k].equalsIgnoreCase(rightRoman)) {
+						rightHit = k;
+					}
+					right = numbers[rightHit];
+				}
+			}
+
+			if (right > current) {
+				current *= -1;
+			}
+			result += current;
+		}
+		return result;
 	}
 }
